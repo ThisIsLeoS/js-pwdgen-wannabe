b1da07a6b6e3c5442810dacc556236f42c353de9
leos <leonardo.siri.email@gmail.com>
2019-10-16 14:52:20 +0200

    complete exercise

---
+++ js/main.js
@@ -0,0 +1,1 @@
+document.writeln("La tua password supersicura è: " + window.prompt("Scrivi il tuo nome") + window.prompt("Scrivi il tuo cognome") + window.prompt("Scrivi il tuo colore preferito") + "19");
